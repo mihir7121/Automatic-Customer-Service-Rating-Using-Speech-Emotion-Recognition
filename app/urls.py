24e27fd7f074283ba4f109cf671dc8aeb6cd374b
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2023-05-03 18:44:40 +0530

    skeleton

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path('upload-audio/', views.upload_audio, name='upload_audio'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.http.response import StreamingHttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AudioForm
+
 import numpy as np
 
 # Create your views here.
-def landing(request):
-    return render(request, "app/landing.html")
-
 def index(request):
     return render(request, "app/index.html")
+
+def upload_audio(request):
+    if request.method == 'POST':
+        form = AudioForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('successfully uploaded')
+    else:
+        form = AudioForm()
+    return render(request, 'app/upload_audio.html', {'form': form})
